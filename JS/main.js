c32dcfc3fdf2a55c19b12e52e86a151128b0dbdb
Graphic <graphic@SOD-SJH-515-14M.local>
2015-11-27 16:50:20 -0500

    add flip css and html

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	var images = [];
+
+	$('box').click(function(){
+			$(this). ().addClass();
+			$("").click(function(){
+
+			}
+	});
+});
+/* flip the pane when hovered */
+	
