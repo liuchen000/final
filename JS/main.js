5a2babb709334b497308c7530a9b46637bb9cadb
liuchen000 <suby715@gmail.com>
2015-12-07 00:28:29 -0500

    week 13 can flip

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,41 @@
-$("#c1").flip('hover'); 
+// var pattern = ['R', 'Y'];
+// var num = 0;
+
+// $('.panel').on('click', function() {
+
+// 	// Does the panel you clicked match the
+// 	// current item in the sequence
+
+// 	var colour = $(this).attr('data-colour');
+
+// 	if ( colour == pattern[num] ) {
+// 		console.log("CORRECT!");
+		
+// 		//Short form of: num = num + 1;
+// 		num++;
+
+// 		if (num > pattern.length-1 ) {
+// 			console.log("YOU WIN!!!!1");
+// 		}
+// 	}
+// 	else {
+// 		console.log("Wrong :(");
+// 	}
+
+// });
+
+
+$(document).ready(function() {
+	
+	$('.sq').click(function(){
+		$(this).css({ opacity: 0 });
+		
+	});
+	
+
+
+});
+
+
+
+
