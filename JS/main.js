ab0e7faf5b43c71dc9c276ce297882d68e612aa2
Graphic <graphic@SOD-SJH-516-26M.local>
2015-12-11 14:00:11 -0500

    week 14, added background music

--- JS/main.js
+++ JS/main.js
@@ -24,14 +24,56 @@
 
 // });
 
-
 $(document).ready(function() {
 	
-	$('.sq').click(function(){
-		$(this).css({ opacity: 0 });
+	// just a delay
+	var dodelay;
+
+	// hold 2 cards that are open
+	var cards = [];
+
+	// remove cards that are incorrect
+	var hidecard = function() {
+		// Flip cards
+		$.each(cards, function(i, $card) {
+			$card.find('.sq').show();
+		});
+		clearInterval(dodelay);
+		cards = [];
+		$('.sq').on('click', doclick)
+	};
+
+
+	var doclick = function(){
+
+		// find the parent
+		var c = $(this).hide().parent();
 		
-	});
+		// Add to the array so we can check against the next one
+		cards.push(c);
+
+		if (cards.length == 2) {
+
+			$('.sq').off('click')
+
+			// If there is a match
+			if (cards[0].attr('data-card') == cards[1].attr('data-card')) {
+				console.log("MATCH!!!!");
+				cards = [];
+				$('.sq').on('click', doclick)
+			}
+			// If there is NO match
+			else {
+				dodelay = setInterval(hidecard, 1000);
+			}
+			
+		}
+	}
+
+	$('.sq').click(doclick);
 	
+
+
 
 
 });
